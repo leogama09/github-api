b33b2b1916d7dc03a7aa3b1c6812e8e4be074488
Leonardo Gama <leogama09@gmail.com>
2023-02-24 15:18:52 -0300

    Update Search.tsx

--- github_finder/src/components/Search.tsx
+++ github_finder/src/components/Search.tsx
@@ -2,7 +2,7 @@ type SearchProps = {
     loadUser: (userName: string) => Promise<void>
 }
 
-import { useState } from 'react'
+import { useState, KeyboardEvent } from 'react'
 
 import {BsSearch} from 'react-icons/bs'
 
@@ -10,6 +10,12 @@ import classes from './Search.module.css'
 
 const Search = ({loadUser}: SearchProps) => {
     const [userName, setUserName] = useState("")
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if(e.key === "Enter") {
+            loadUser(userName)
+        }
+    }
 
   return (
     <div className={classes.search}>
@@ -20,6 +26,7 @@ const Search = ({loadUser}: SearchProps) => {
                 type="text" 
                 placeholder='Digite o nome do usuário' 
                 onChange={(e) => setUserName(e.target.value)}
+                onKeyDown={handleKeyDown}
             />
             <button onClick={() => loadUser(userName)}>
                 <BsSearch />
